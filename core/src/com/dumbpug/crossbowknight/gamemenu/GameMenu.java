2168c8f55923cd92af7cf07e308cdc8a12e6a653
nikkorn <nikhoward1234@hotmail.co.uk>
2016-10-17 00:24:22 +0100

    Made the knight more knighty. Added classes for the game menu.

---
+++ core/src/com/dumbpug/crossbowknight/gamemenu/GameMenu.java
@@ -0,0 +1,7 @@
+package com.dumbpug.crossbowknight.gamemenu;
+
+/**
+ * Created by nik on 16/10/16.
+ */
+public class GameMenu {
+}
---
+++ core/src/com/dumbpug/crossbowknight/gamemenu/GameMenuDrawer.java
@@ -0,0 +1,8 @@
+package com.dumbpug.crossbowknight.gamemenu;
+
+/**
+ * Responsible for drawing a game menu.
+ * Created by nik on 16/10/16.
+ */
+public class GameMenuDrawer {
+}
